dcc471021b800aa713724c5beb0bbaebab57b8d8
Excali8ur <rianne.boumans@zuyd.nl>
2020-03-27 10:26:08 +0100

    Test Master

--- ClassLibrary/GUITest/Form1.cs
+++ ClassLibrary/GUITest/Form1.cs
@@ -15,12 +15,12 @@ namespace GUITest
 {
     public partial class Form1 : Form
     {       
-
+	//TEST MASTER BRANCHE
         public Form1()
         {
             InitializeComponent();
         }
-
+	//EXTRA BERICHT VOOR MERGE TEST
         private void btnTest_Click(object sender, EventArgs e)
         {
             User u = new User();
